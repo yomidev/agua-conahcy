e2374e207b1155876f2cc5ee7905d9f966fedb87
Yomira Martinez <yomimsm@gmail.com>
2025-03-06 17:09:18 -0600

    Assets

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -13,8 +13,8 @@
     <!--<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
-    <!--<link rel="stylesheet" href="{{asset('build/assets/app-WJUnvubz.css')}}">
-    <script src="{{asset('build/assets/app-Bj6lTi1Z.js')}}"></script>-->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('build/assets/app-WJUnvubz.css')}}">
+    <script src="{{asset('build/assets/app-CbdEbqj6.js')}}"></script>
+
 </head>
 <body>
