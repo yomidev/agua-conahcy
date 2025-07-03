b63a448388558252d1bf61e7c6007b92357ac263
Soporte Técnico de Educación a Distancia <103531425+soporteead@users.noreply.github.com>
2025-07-03 13:09:44 -0600

    Documents Download

--- resources/views/congreso.blade.php
+++ resources/views/congreso.blade.php
@@ -5,7 +5,7 @@
 <nav id="logos">
     <div class="container-fluid d-flex justify-content-between align-items-center pt-2 logos-congreso">
         <img src="{{asset('pictures/gob-2.png')}}" alt="" class="img-fluid" style="max-width: 125px">
-        <img src="{{asset('pictures/sep-2.png')}}" alt=""  class="img-fluid" style="max-width: 200px">
+        <img src="{{asset('pictures/sep-2.png')}}" alt="" class="img-fluid" style="max-width: 200px">
         <img src="{{asset('pictures/2.png')}}" alt="" class="img-fluid" style="max-width: 300px">
         <img src="{{asset('pictures/3.png')}}" alt="" class="img-fluid" style="max-width: 200px">
         <img src="{{asset('pictures/logo-tecnm.png')}}" alt="img-fluid" style="max-width: 100px">
@@ -15,17 +15,25 @@
         <img src="{{asset('pictures/cintillo.jpg')}}" alt="" class="img-fluid">
     </div>
 </nav>
-<nav id="enlaces" class="navbar navbar-expand-lg bg-body-tertiary navbar-dark" style="background-color:#344474 !important">
+<nav id="enlaces" class="navbar navbar-expand-lg bg-body-tertiary navbar-dark"
+    style="background-color:#344474 !important">
     @include('templates.partials.menu-links')
 </nav>
 <main>
     <div>
         <img src="{{asset('pictures/banner-congreso.png')}}" alt="" class="img-fluid">
     </div>
-    <div id="information-congreso" class="container-fluid d-flex justify-content-between align-items-center p-5 flex-wrap" style="background-color:#344474 !important">
-        <h3 class="text-center text-white" style="letter-spacing:2px"><span class="text-uppercase mb-3" style="font-weight:900 !important; display:inline-block">Fecha</span><br>25 y 26 de <br>Septiembre de 2025</h3>
-        <h3 class="text-center text-white" style="letter-spacing:2px"><span class="text-uppercase mb-3" style="font-weight:900 !important; display:inline-block">Sede</span> <br>Instituto Tecnológico <br>de Tijuana</h3>
-        <h3 class="text-center text-white" style="letter-spacing:2px"><span class="text-uppercase mb-3" style="font-weight:900 !important; display:inline-block">Modalidad </span><br>Híbrida</h3>
+    <div id="information-congreso"
+        class="container-fluid d-flex justify-content-between align-items-center p-5 flex-wrap"
+        style="background-color:#344474 !important">
+        <h3 class="text-center text-white" style="letter-spacing:2px"><span class="text-uppercase mb-3"
+                style="font-weight:900 !important; display:inline-block">Fecha</span><br>25 y 26 de <br>Septiembre de
+            2025</h3>
+        <h3 class="text-center text-white" style="letter-spacing:2px"><span class="text-uppercase mb-3"
+                style="font-weight:900 !important; display:inline-block">Sede</span> <br>Instituto Tecnológico <br>de
+            Tijuana</h3>
+        <h3 class="text-center text-white" style="letter-spacing:2px"><span class="text-uppercase mb-3"
+                style="font-weight:900 !important; display:inline-block">Modalidad </span><br>Híbrida</h3>
     </div>
     <!--contador-->
     <div class="container d-flex justify-content-between countdown-box p-3" style="gap:10px">
@@ -49,9 +57,9 @@
     <div class="slider mt-4 align-items-center" style="height: 200px !important">
         <div class="slider-track" style="height: 200px !important">
             @foreach($images as $img)
-                <div class="slide text-center align-items-center" style="height: 200px !important">
-                    <img src="{{asset($img['src'])}}" alt="logo"class="w-75">
-                </div>
+            <div class="slide text-center align-items-center" style="height: 200px !important">
+                <img src="{{asset($img['src'])}}" alt="logo" class="w-75">
+            </div>
             @endforeach
         </div>
     </div>
@@ -62,11 +70,13 @@
                     <div class="carousel-inner carousel-inner-3"></div>
 
                     <!-- Controles del Carousel -->
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Anterior</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Siguiente</span>
                     </button>
@@ -75,8 +85,12 @@
             <div class="col-md-6">
                 <h4 class="title">Objetivo</h4>
                 <p style="text-align: justify;" class="padding-responsive fs-5">
-                    Generar espacios de diálogo, análisis, intercambio y formación, entre diversos agentes de la sociedad sobre las
-                    <b>contribuciones de la investigación</b> en temas relacionados con el cuidado del agua, los métodos de tratamiento de agua potable y residual, así como las posibles alternativas para la recuperación y reúso del vital líquido. Además de la implementación de tecnologías emergentes en el tratamiento y monitoreo de la calidad del agua.
+                    Generar espacios de diálogo, análisis, intercambio y formación, entre diversos agentes de la
+                    sociedad sobre las
+                    <b>contribuciones de la investigación</b> en temas relacionados con el cuidado del agua, los métodos
+                    de tratamiento de agua potable y residual, así como las posibles alternativas para la recuperación y
+                    reúso del vital líquido. Además de la implementación de tecnologías emergentes en el tratamiento y
+                    monitoreo de la calidad del agua.
                 </p>
             </div>
         </div>
@@ -119,21 +133,39 @@
         </div>
     </div>
     <div class="container mt-5">
-        <h4 class="title text-center">Envío de Trabajos</h4>
+        <h4 class="title text-center">PARTICIPA EN EL CONGRESO</h4>
+        <p style="font-size:20px; text-align:center">Registra tu trabajo para <b>presentación oral o en cartel</b> del
+            <b>1 al 30 de Julio
+                de 2025.</b>
+            <br>O regístrate como asistente del <b>1 de Julio al 23 de Septiembre de 2025</b> para poder conocer las
+            tendencias actuales en el tema del agua y obtener constancia de participación.
+        </p>
         <div class="row align-items-start mt-3">
             <div class="col-md-6">
-                <ol class="padding-responsive-ol fs-5" style="text-align: justify;">
+                <a href="{{ asset('documents/pago_congreso.pdf') }}" target="_blank" download
+                    class="btn btn-primary fw-bold"
+                    style="background-color:#621132 !important; border:none; font-size: 30px; width: 80%; border-radius: 25px">Cuotas
+                    de Inscripción y Solicitud de recibo</a>
+                <br><br>
+                <a href="{{ asset('documents/hospedaje_tijuana.pdf') }}" target="_blank" download
+                    class="btn btn-primary fw-bold"
+                    style="background-color:#621132 !important; border:none; font-size: 30px; width: 80%; border-radius: 25px">Hospedaje</a>
+                <!--<ol class="padding-responsive-ol fs-5" style="text-align: justify;">
                     <li>Descargar el formato para envío de trabajos, ingresando en la siguiente liga o escaneando el código QR <a href="https://bit.ly/congreso_agualys" target="_blank" class="fw-bold">https://bit.ly/congreso_agualys</a></li>
                     <li>Enviar el trabajo al correo: <b>agenda_agua@aguascalientes.tecnm.mx</b></li>
                     <li>La fecha límite para recepción de trabajos es el <b>30 de Mayo de 2025</b></li>
                     <li>La notificación de trabajos aceptados se realizará vía correo electrónico a partir del <b>20 de junio de 2025</b></li>
                     <li>Si el trabajo es aceptado, deberá de registrarse en la página del congreso <a href="{{ route('registro') }}" target="_blank" class="fw-bold">https://lanalimsa.aguascalientes.tecnm.mx/congreso/registro</a> del <b>20 de junio al 31 de julio de 2025</b>.</li>
-                </ol>
+                </ol>-->
             </div>
             <div class="col-md-6 text-center">
-                <a href="{{ route('registro') }}" class="btn btn-primary fw-bold" target="_blank" style="background-color:#344474 !important; border:none; font-size: 40px; width: 80%; height: 80px; border-radius: 25px">REGISTRO</a><br>
-                <br><br><br><br><br><br><h2>Registro para Directivos</h2>
-                <a href="{{ route('registro.directivos') }}" class="btn btn-primary btn-danger fw-bold" target="_blank" style="border:none; font-size: 40px; width: 80%; height: 80px; border-radius: 25px">CLICK AQUI</a><br>
+                <a href="{{ route('registro') }}" class="btn btn-primary fw-bold" target="_blank"
+                    style="background-color:#344474 !important; border:none; font-size: 40px; width: 80%; height: 80px; border-radius: 25px">REGISTRO</a><br>
+                <br><br><br><br><br><br>
+                <h2>Registro para Directivos</h2>
+                <a href="{{ route('registro.directivos') }}" class="btn btn-primary btn-danger fw-bold" target="_blank"
+                    style="border:none; font-size: 40px; width: 80%; height: 80px; border-radius: 25px">CLICK
+                    AQUI</a><br>
                 <!--<img src="{{asset('pictures/qrcode_3Lx2eXTadT.png')}}" alt="" class="img-fluid w-50"><br>
                 <a href="https://bit.ly/congreso_agualys" class="btn btn-primary fw-bold" target="_blank" style="background-color:#344474 !important; border:none">Descargar plantilla</a>-->
             </div>
@@ -161,45 +193,57 @@
                 <div class="card-body">
                     <!--<p>Título de la plenaria</p>-->
                     <p class="fw-bold">Dr. Satinder <br>Kaur Brar
-                        <br><img src="{{asset('pictures/canada.png')}}" alt="Canadá" class="img-fluid" style="max-width: 50px">
+                        <br><img src="{{asset('pictures/canada.png')}}" alt="Canadá" class="img-fluid"
+                            style="max-width: 50px">
                     </p>
-                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important" data-bs-toggle="modal" data-bs-target="#exampleModal" data-researcher="satinder_kaur_brar">Curriculum Vitae</a>
+                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal"
+                        data-researcher="satinder_kaur_brar">Curriculum Vitae</a>
                 </div>
             </div>
             <div class="card plenary-card text-center">
                 <img src="{{asset('pictures/eva.jpg')}}" alt="" class="img-fluid">
                 <div class="card-body">
                     <p class="fw-bold">Dra. Eva Rose <br>Kozak
-                        <br><img src="{{asset('pictures/bandera.png')}}" alt="México" class="img-fluid" style="max-width: 50px">
+                        <br><img src="{{asset('pictures/bandera.png')}}" alt="México" class="img-fluid"
+                            style="max-width: 50px">
                     </p>
-                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important" data-bs-toggle="modal" data-bs-target="#exampleModal2" data-researcher="eva_rose_kozak">Curriculum Vitae</a>
+                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal2"
+                        data-researcher="eva_rose_kozak">Curriculum Vitae</a>
                 </div>
             </div>
             <div class="card plenary-card text-center">
                 <img src="{{asset('pictures/natalie.png')}}" alt="" class="img-fluid">
                 <div class="card-body">
                     <p class="fw-bold">Dra. Natalie <br>Mladenov
-                        <br><img src="{{asset('pictures/estados-unidos-de-america.png')}}" alt="Estados Unidos" class="img-fluid" style="max-width: 50px">
+                        <br><img src="{{asset('pictures/estados-unidos-de-america.png')}}" alt="Estados Unidos"
+                            class="img-fluid" style="max-width: 50px">
                     </p>
-                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important" data-bs-toggle="modal" data-bs-target="#exampleModal3">Curriculum Vitae</a>
+                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal3">Curriculum Vitae</a>
                 </div>
             </div>
             <div class="card plenary-card text-center">
                 <img src="{{asset('pictures/amy.jpg')}}" alt="" class="img-fluid">
                 <div class="card-body">
                     <p class="fw-bold">Prof. Amy M. <br>Bilton
-                        <br><img src="{{asset('pictures/canada.png')}}" alt="Canadá" class="img-fluid" style="max-width: 50px">
+                        <br><img src="{{asset('pictures/canada.png')}}" alt="Canadá" class="img-fluid"
+                            style="max-width: 50px">
                     </p>
-                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important" data-bs-toggle="modal" data-bs-target="#exampleModal4">Curriculum Vitae</a>
+                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal4">Curriculum Vitae</a>
                 </div>
             </div>
             <div class="card plenary-card text-center">
                 <img src="{{asset('pictures/rachelgomes.jpg')}}" alt="" class="img-fluid">
                 <div class="card-body">
                     <p class="fw-bold">Prof. Rachel Louise Gomes
-                        <br><img src="{{asset('pictures/reino-unido.png')}}" alt="Inglaterra" class="img-fluid" style="max-width: 50px">
+                        <br><img src="{{asset('pictures/reino-unido.png')}}" alt="Inglaterra" class="img-fluid"
+                            style="max-width: 50px">
                     </p>
-                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important" data-bs-toggle="modal" data-bs-target="#exampleModal5">Curriculum Vitae</a>
+                    <a class="btn btn-primary" style="background-color:#344474 !important; border:none !important"
+                        data-bs-toggle="modal" data-bs-target="#exampleModal5">Curriculum Vitae</a>
                 </div>
             </div>
         </div>
@@ -221,4 +265,3 @@
     @include('templates.partials.footer')
     <div class="img-footer"></div>
 </footer>
-
