23e04be9bd78efd87cc82a5aab87d9111a8e7b6f
Soporte Técnico de Educación a Distancia <103531425+soporteead@users.noreply.github.com>
2025-07-10 08:09:31 -0600

    add field to excel

--- app/Exports/CongressExport.php
+++ app/Exports/CongressExport.php
@@ -25,20 +25,35 @@ class CongressExport implements FromCollection, WithHeadings
                 '4' => 'Taller y Sesión Paralela',
             ];
 
+            $modality = [
+                '1' => 'Presencial',
+                '2' => 'Virtual',
+                '3' => 'Híbrida',
+            ];
+            $logistics = [
+                '1' => 'Sí',
+                '0' => 'No',
+            ];
+
             return [
                 'Nombre' => $congress->name,
                 'Apellidos' => $congress->lastname,
                 'Correo Electrónico' => $congress->email,
+                'Teléfono' => $congress->phone,
                 'Institución' => $congress->institution,
                 'Tipo de Participación' => $participation[$congress->participation] ?? 'Desconocido',
                 'Título de la Ponencia' => $congress->presentation,
                 'Área Temática' => $congress->area,
+                'Modalidad de Asistencia' => $modality[$congress->modality] ?? 'Desconocido',
+                '¿Requiere apoyo de hospedaje?' => $logistics[$congress->logistics] ?? 'Desconocido',
+                '¿Tiene alguna restricción alimentaria o necesidad especial?' => $congress->food ?? 'No especificado',
+                'Idioma Preferido' => $congress->language ?? 'Desconocido',
             ];
         });
     }
 
     public function headings(): array
     {
-        return ['Nombre', 'Apellidos', 'Correo Electrónico', 'Institución', 'Tipo de Participación', 'Título de la Ponencia', 'Área Temática'];
+        return ['Nombre', 'Apellidos', 'Correo Electrónico', 'Teléfono', 'Institución', 'Tipo de Participación', 'Título de la Ponencia', 'Área Temática', 'Modalidad de Asistencia', '¿Requiere apoyo de hospedaje?', '¿Tiene alguna restricción alimentaria o necesidad especial?', 'Idioma Preferido'];
     }
 }
